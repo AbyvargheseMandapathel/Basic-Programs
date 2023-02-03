21a2f1d5974edaf1ab5814d8f3f6dc79d0f10e74
Aby varghese <56151184+AbyvargheseMandapathel@users.noreply.github.com>
2023-02-03 14:39:47 +0530

    Prime Numbers in C

---
+++ c program/Prime Numbers.c
@@ -0,0 +1,27 @@
+//Prime Numbers in C
+
+#include <stdio.h>
+int main() {
+    int iterations, number ,flag=0;
+    
+    printf("enter the number to check");
+    scanf("%d",&number);
+    
+    for (iterations=2;iterations<=number/2;iterations++) {
+        if (number % iterations == 0){
+            flag =1;
+        }
+    }
+    if (number == 1) {
+        printf("Number is not prime");
+    }
+    else if (number == 0) {
+        printf("Number is neither prime not nor");
+    }
+    else if (flag == 0) {
+        printf("Number is  prime");
+    }
+    else {
+        printf("Number is not  prime");
+    }
+    }
